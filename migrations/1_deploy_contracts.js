98369c89e2272a6b9f5366d42dcfd9416eaa987e
Gregory Kennedy <peacedude@gmail.com>
2018-03-05 18:39:15 -0800

    Update 1_deploy_contracts.js

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,1 +1,18 @@
+var SBCoin = artifacts.require('./SBCoin.sol');
+var SBCDistribution = artifacts.require('./PolyDistribution.sol');
 
+module.exports = async (deployer, network) => {
+  let _now = Date.now();
+  let _fromNow = 129600 * 1000; // Start distribution in 1 hour
+  let _startTime = (_now + _fromNow) / 1000;
+  await deployer.deploy(SBCDistribution, _startTime);
+  console.log(`
+    ---------------------------------------------------------------
+    --------- POLYMATH (POLY) TOKEN SUCCESSFULLY DEPLOYED ---------
+    ---------------------------------------------------------------
+    - Contract address: ${SBCDistribution.address}
+    - Distribution starts in: ${_fromNow/1000/60} minutes
+    - Local Time: ${new Date(_now + _fromNow)}
+    ---------------------------------------------------------------
+  `);
+};
