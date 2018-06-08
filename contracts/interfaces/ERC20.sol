a326cc3268d3a8462ca20fd4ba78e8a3b841a15b
Gregory Kennedy <peacedude@gmail.com>
2018-06-08 12:44:38 -0700

    Update and rename IERC20.sol to ERC20.sol

--- contracts/interfaces/ERC20.sol
+++ contracts/interfaces/ERC20.sol
@@ -1,7 +1,7 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.23;
 
 /// ERC Token Standard #20 Interface (https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md)
-interface IERC20 {
+interface ERC20 {
     function balanceOf(address _owner) public view returns (uint256 balance);
     function transfer(address _to, uint256 _value) public returns (bool success);
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
